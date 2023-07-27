193460fc806d470476e26d3d9924b221f99874db
Gabriel Reyes <greyesgu@pichincha.com>
2023-07-27 03:37:40 -0500

    first commit

---
+++ src/main/java/com/pichincha/mambu/marvel/comic/MamMsaMarvelComicApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.mambu.marvel.comic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamMsaMarvelComicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamMsaMarvelComicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichincha/mambu/marvel/comic/MamMsaMarvelComicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.mambu.marvel.comic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamMsaMarvelComicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
